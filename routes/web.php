9a18d427f5b36387af2ee2eff03d8094e4fe10c1
syawqihacking <muhammadsyauqi930@gmail.com>
2025-05-08 10:04:15 +0700

    update dulu gak sih

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,9 @@ Route::middleware(['auth',AuthAdmin::class])->group(function(){
     Route::get('/admin/products', [AdminController::class, 'products'])->name('admin.products');
     Route::get('/admin/products/add', [AdminController::class, 'products_add'])->name('admin.product.add');
     Route::post('/admin/products/store', [AdminController::class, 'products_store'])->name('admin.product.store');
+    Route::get('/admin/product/{id}/edit', [AdminController::class, 'product_edit'])->name('admin.product.edit');
+    Route::put('/admin/product/update', [AdminController::class, 'product_update'])->name('admin.product.update');
+    Route::delete('/admin/product/{id}/delete', [AdminController::class, 'product_delete'])->name('admin.product.delete');
 
 
 });
